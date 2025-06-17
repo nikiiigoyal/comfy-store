194b6f637fecc52964731b575b894bb681e854af
nikiiigoyal <nikiiigoyal@gmail.com>
2025-06-17 08:48:07 +0530

    Modified ABout page added errorelement there

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import {
   Checkout,
   Orders,
 } from './pages';
+import ErrorElement from "./components/ErrorElement";
 
 
 const router = createBrowserRouter([
@@ -39,7 +40,11 @@ const router = createBrowserRouter([
         path: 'cart',
         element: <Cart />,
       },
-      { path: 'about', element: <About /> },
+      { path: 'about',
+         element: <About />,
+         errorElement: <ErrorElement />
+        
+        },
       {
         path: 'checkout',
         element: <Checkout />,
