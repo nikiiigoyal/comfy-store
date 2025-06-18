165907ed2b021db8128c763c43e4a30536a9198a
nikiiigoyal <nikiiigoyal@gmail.com>
2025-06-18 11:50:56 +0530

    Add in types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -44,3 +44,29 @@ export type Params =  {
 }
 
 export type ProductsResponseWithParams = ProductsResponse & { params: Params };
+
+export type SingleProductResponse = {
+    data: Product;
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+    meta : {};
+};
+
+export type CartItem = {
+  cartID: string;
+  productID: number;
+  image: string;
+  title: string;
+  price: string;
+  amount: number;
+  productColor: string;
+  company: string;
+};
+
+export type CartState = {
+  cartItems: CartItem[];
+  numItemsInCart: number;
+  cartTotal: number;
+  shipping: number;
+  tax: number;
+  orderTotal: number;
+};
