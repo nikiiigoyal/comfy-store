f99dba2f64002bb5574267219a79dd15c9012ff7
nikiiigoyal <nikiiigoyal@gmail.com>
2025-06-18 11:51:29 +0530

    Add in package and main

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -1,12 +1,27 @@
-import { createSlice} from '@reduxjs/toolkit';
+// import { createSlice} from '@reduxjs/toolkit';
+// import type { CartState } from '../../utils/types';
 
-const initialState = {
-  name: 'cart slice',
-};
-const cartSlice = createSlice({
-  name: 'cart',
-  initialState,
-  reducers: {},
-});
+// const initialState = {
+//   name: 'cart slice',
+// };
+// const getCartFromLocalStorage = () : CartState => {
+//   const cart = localStorage.getItem('cart');
+//  return cart ? JSON.parse(cart) : defaultState;
+// }
+// const cartSlice = createSlice({
+//   name: 'cart',
+//   initialState: getCartFromLocalStorage(),
+//   reducers: {
+//     addItem: (state, action: PayloadAction<CartItem>) => {
+//       const newCartItem = action.payload;
+//       const item = state.cartItems.find((i) => i.cartID === newCartItem.cartID)
+//       if (item) {
+//         item.amount += newCartItem.amount;
+//       } else {
+//         state.cartItems.push(newCartItem);
+//       }
+//       state.numItemsInCart += new CartIte    }
+//   },
+// });
 
-export default cartSlice.reducer;
+// export default cartSlice.reducer;
--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import './index.css'
 import App from './App'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import { Toaster } from './components/ui/toaster'
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
+    <Toaster />
     <App />
   </Provider >,
 )
