e0faab72ab031aeee21075c9f7321a9241120af5
nikiiigoyal <nikiiigoyal@gmail.com>
2025-06-18 11:50:23 +0530

    Add single product its color and amount

--- src/components/PaginationContainer.tsx
+++ src/components/PaginationContainer.tsx
@@ -1,8 +1,49 @@
+import { useLoaderData, useLocation } from "react-router-dom";
+import type { ProductsResponseWithParams } from "../utils/types";
+import { PaginationItem, PaginationLink,PaginationNext,PaginationContent,Pagination,PaginationPrevious } from "./ui/pagination";
+import { constructPrevOrNextUrl, constructUrl } from "../utils/pagination";
+
 const PaginationContainer = () => {
+    const { meta } = useLoaderData() as ProductsResponseWithParams;
+  const { pageCount, page } = meta.pagination;
+
+  const { search, pathname } = useLocation();
+
+  const pages = Array.from({ length: pageCount }, (_, index) => index + 1);
+
+  if (pageCount < 2) return null;
+
+  const renderPagination = pages.map((pageNumber) => {
+    const isActive = pageNumber === page;
+    const url = constructUrl({ pageNumber, search, pathname });
+
     return (
-        <>
+      <PaginationItem key={pageNumber}>
+        <PaginationLink to={url} isActive={isActive}>
+          {pageNumber}
+        </PaginationLink>
+      </PaginationItem>
+    );
+  });
+  const { prevUrl, nextUrl } = constructPrevOrNextUrl({
+    currentPage: page,
+    pageCount,
+    search,
+    pathname,
+  });
+  return (
+    <Pagination className='mt-16'>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious to={prevUrl} />
+        </PaginationItem>
+        {renderPagination}
+        <PaginationItem>
+          <PaginationNext to={nextUrl} />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
         
-        </>
     )
 }
 export default PaginationContainer;
