a0409a0b4b9ed218e34af5c6ded8c77c18c790f5
nikiiigoyal <nikiiigoyal@gmail.com>
2025-06-19 11:42:13 +0530

    congigured pakcge

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,7 +1,35 @@
-function Checkout () {
+import { redirect, type LoaderFunction } from "react-router-dom"
+import type { ReduxStore } from "../store"
+import { toast } from "../components/ui/use-toast";
+import SectionTitle from "../components/SectionTitle";
+import { useAppSelector } from "../hooks";
+import CartTotals from "../components/CartTotals";
+
+export const loader =
+  (store: ReduxStore): LoaderFunction =>
+  async (): Promise<Response | null> => {
+    const user = store.getState().userState.user;
+    if (!user) {
+      toast({ description: 'Please login to continue' });
+      return redirect('/login');
+    }
+
+    return null;
+  };
+
+
+    function Checkout () {
+        const cartTotal = useAppSelector((state) => state.cartState.cartTotal);
+  if (cartTotal === 0) {
+    return <SectionTitle text='Your cart is empty' />;
+  }
     return (
         <>
-        
+        <SectionTitle text='Place your order' />
+      <div className='mt-8 grid gap-8  md:grid-cols-2 items-start'>
+        <CheckoutForm />
+        <CartTotals />
+      </div>
         </>
     )
 }
--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,8 +1,49 @@
-function Orders () {
-    return (
-        <>
-        
-        </>
-    )
-}
-export default Orders
+import { redirect, useLoaderData, type LoaderFunction } from "react-router-dom";
+import type { ReduxStore } from "../store";
+import type { OrdersResponse } from "../utils/types";
+import { toast } from "../components/ui/use-toast";
+import { customFetch } from "../utils/customFetch";
+import SectionTitle from "../components/SectionTitle";
+
+export const loader =
+  (store: ReduxStore): LoaderFunction =>
+  async ({ request }): Promise<OrdersResponse | Response | null> => {
+    const user = store.getState().userState.user;
+
+    if (!user) {
+      toast({ description: 'Please login to continue' });
+      return redirect('/login');
+    }
+    const params = Object.fromEntries([
+      ...new URL(request.url).searchParams.entries(),
+    ]);
+
+    try {
+      const response = await customFetch.get<OrdersResponse>('/orders', {
+        params,
+        headers: {
+          Authorization: `Bearer ${user.jwt}`,
+        },
+      });
+
+      return { ...response.data };
+    } catch (error) {
+      console.log(error);
+      toast({ description: 'Failed to fetch orders' });
+      return null;
+    }
+  };
+const Orders = () => {
+  const { meta } = useLoaderData() as OrdersResponse;
+  if (meta.pagination.total < 1) {
+    return <SectionTitle text='Please make an order' />;
+  }
+  return (
+    <>
+      <SectionTitle text='Your Orders' />
+      <OrdersList />
+      <ComplexPaginationContainer />
+    </>
+  );
+};
+export default Orders;
