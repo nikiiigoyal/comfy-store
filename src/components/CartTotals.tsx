3e54c082a902e7d6888d97ca62e72e673948d8ca
nikiiigoyal <nikiiigoyal@gmail.com>
2025-06-19 09:50:46 +0530

    Add new file carttotals

---
+++ src/components/CartTotals.tsx
@@ -0,0 +1,33 @@
+import { useAppSelector } from "../hooks"
+import { FormatAsDollars } from "../utils/formatAsDollars";
+import { Card, CardTitle } from "./ui/card";
+import { Separator } from "./ui/separator";
+
+const CartTotals = () => {
+    const {cartTotal, shipping, tax, orderTotal} = useAppSelector(
+        (state) => state.cartState
+    );
+    return(
+        <Card  className="p-8 bg-muted">
+            <CartTotalRow label='Subtotal' amount={cartTotal} />
+            <CartTotalRow label='Shipping' amount={shipping} />
+            <CartTotalRow label='Tax' amount={tax} />
+            <CardTitle className="mt-8">
+                <CartTotalRow label='Order Total' amount={orderTotal} lastRow />
+            </CardTitle>
+        </Card>
+    )
+}
+function CartTotalRow ({label,amount,lastRow}: {label: string; amount: number; lastRow?: boolean;
+    }) {
+        return (
+            <>
+            <p className='flex justify-between text-sm'>
+        <span>{label}</span>
+        <span>{FormatAsDollars(amount)}</span>
+      </p>
+      {lastRow ? null : <Separator className='my-2' />}
+            </>
+        )
+    }
+    export default CartTotals;
