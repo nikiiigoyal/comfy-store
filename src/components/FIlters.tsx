11347a3f11d40ded19597f4f98418723621c058b
nikiiigoyal <nikiiigoyal@gmail.com>
2025-06-25 22:12:45 +0530

    Add filters

--- src/components/FIlters.tsx
+++ src/components/FIlters.tsx
@@ -9,58 +9,67 @@ import SelectInput from "./FormSelect";
 import { Button } from "./ui/button";
 
 const Filters = () => {
-     const { meta, params } = useLoaderData() as ProductsResponseWithParams;
+  const { meta, params } = useLoaderData() as ProductsResponseWithParams;
   const { search, company, category, shipping, order, price } = params;
 
-    return (
-        <>
-        <Form className="border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
-            {/* search */}
-            <FormInput label="search product" name="search" defaultValue={search} type="search"
-            />
-            
-           {/*categories  */}
-<SelectInput
-  label='select category'
-  name='category'
-  options={meta.categories}
-  defaultValue={category}
-/>;
-{
-  /* COMPANIES */
-}
-<SelectInput
-  label='select company'
-  name='company'
-  options={meta.companies}
-  defaultValue={company}
-/>;
-{
-  /* ORDER */
-}
+  // Add default "All" option to categories
+  const categoryOptions = ['All', ...(meta.categories || [])];
+  const companyOptions = ['All', ...(meta.companies || [])];
 
-<SelectInput
-  label='order by'
-  name='order'
-  options={['a-z', 'z-a', 'high', 'low']}
-  defaultValue={order}
-/>
+  return (
+    <Form className="border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
+      {/* Search */}
+      <FormInput 
+        label="search product" 
+        name="search" 
+        defaultValue={search} 
+        type="search"
+      />
+      
+      {/* Categories */}
+      <SelectInput
+        label='select category'
+        name='category'
+        options={categoryOptions}
+        defaultValue={category || 'All'}
+      />
 
-{/* price */}
-<FormRange 
-label="price" name="price" defaultValue={price}/>
+      {/* Companies */}
+      <SelectInput
+        label='select company'
+        name='company'
+        options={companyOptions}
+        defaultValue={company || 'All'}
+      />
 
-{/* shipping */}
-<FormCheckbox label="free shipping" name="shipping" defaultValue={shipping}/>
- <Button type='button'
+      {/* Order */}
+      <SelectInput
+        label='order by'
+        name='order'
+        options={['a-z', 'z-a', 'high', 'low']}
+        defaultValue={order}
+      />
+
+      {/* Price */}
+      <FormRange label="price" name="price" defaultValue={price}/>
+
+      {/* Shipping */}
+      <FormCheckbox 
+        label="free shipping" 
+        name="shipping" 
+        defaultValue={shipping}
+      />
+
+      <Button 
+        type='button'
         asChild
         size='sm'
         variant='outline'
-        className='self-end mb-2'>
-            <Link to='/products'>reset</Link>
-        </Button> 
-        </Form>
-        </>
-    )
+        className='self-end mb-2'
+      >
+        <Link to='/products'>reset</Link>
+      </Button> 
+    </Form>
+  );
 }
 export default Filters;
