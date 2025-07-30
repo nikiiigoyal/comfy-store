3447b1e565da41662b333145dfeb2708ec36b9ce
nikiiigoyal <nikiiigoyal@gmail.com>
2025-07-30 10:41:01 +0530

    Modifyy app page

--- src/components/FIlters.tsx
+++ src/components/FIlters.tsx
@@ -291,7 +291,7 @@ const Filters = () => {
                 </select>
               </div>
 
-              {/* Sort */}
+              
               <div>
                 <label className="block text-sm font-medium text-gray-700 mb-1">
                   Sort By
